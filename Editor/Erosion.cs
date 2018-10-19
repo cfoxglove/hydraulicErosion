2bbe4c694b91ef6b1969b373ac169299932b7607
Chad Foxglove <chad@unity3d.com>
2018-10-19 12:00:09 -0700

    refactor

--- Editor/Erosion.cs
+++ Editor/Erosion.cs
@@ -2,6 +2,8 @@
 using UnityEditor;
 
 public class Erosion : EditorWindow {
+    private bool m_Init = false;
+
     [SerializeField]
     private ComputeShader m_ComputeShader = null;
     [SerializeField]
@@ -13,19 +15,21 @@ public class Erosion : EditorWindow {
     [SerializeField]
     private float m_DeltaTime = 0.01f;
     [SerializeField]
-    private float m_PrecipRate = 10.0f;
+    private float m_PrecipRate = 0.05f;
+    [SerializeField]
+    private float m_FlowRate = -0.05f;
 
     private int[] m_texDim = { 256, 256 };
-
-    private RenderTexture m_ResultRT;
+    
     private RenderTexture m_InHeightRT;
     private RenderTexture m_PrecipMaskRT;
     
     private RenderTexture m_WaterRT;
     private RenderTexture m_WaterVelRT;
-    private RenderTexture m_WaterVelPrevRT;
 
     private RenderTexture m_FluxRT;
+
+    
 
     /*
     private RenderTexture m_LoamRT;
@@ -43,86 +47,48 @@ public class Erosion : EditorWindow {
     }
 
     void InitData() {
-        //create the result texture we'll be writing to from our compute shader
-        if (m_ResultRT == null) {
-            m_ResultRT = new RenderTexture(m_texDim[0], m_texDim[1], 0);
-            m_ResultRT.format = RenderTextureFormat.ARGBFloat;
-            m_ResultRT.enableRandomWrite = true;
-            m_ResultRT.Create();
-        }
-        if(m_InHeightRT == null) {
-            m_InHeightRT = new RenderTexture(m_texDim[0], m_texDim[1], 0);
-            m_InHeightRT.enableRandomWrite = true;
-            m_InHeightRT.Create();
-        }
-        if(m_PrecipMaskRT == null) {
-            m_PrecipMaskRT = new RenderTexture(m_texDim[0], m_texDim[1], 0);
-            m_PrecipMaskRT.enableRandomWrite = true;
-            m_PrecipMaskRT.Create();
-        }
+        if(m_Init == true) { return; }
+
+        m_InHeightRT = new RenderTexture(m_texDim[0], m_texDim[1], 0);
+        m_InHeightRT.format = RenderTextureFormat.RFloat;
+        m_InHeightRT.enableRandomWrite = true;
+        m_InHeightRT.Create();
+
+        m_PrecipMaskRT = new RenderTexture(m_texDim[0], m_texDim[1], 0);
+        m_PrecipMaskRT.enableRandomWrite = true;
+        m_PrecipMaskRT.Create();
 
         //Water level
-        if (m_WaterRT == null) {
-            m_WaterRT = new RenderTexture(m_texDim[0], m_texDim[1], 0);
-            m_WaterRT.format = RenderTextureFormat.RFloat;
-            m_WaterRT.enableRandomWrite = true;
-            m_WaterRT.Create();
-        }
+        m_WaterRT = new RenderTexture(m_texDim[0], m_texDim[1], 0);
+        m_WaterRT.format = RenderTextureFormat.RFloat;
+        m_WaterRT.enableRandomWrite = true;
+        m_WaterRT.Create();
 
         //water velocity
-        if(m_WaterVelPrevRT == null) {
-            m_WaterVelPrevRT = new RenderTexture(m_texDim[0], m_texDim[1], 0);
-            m_WaterVelPrevRT.format = RenderTextureFormat.RGFloat;
-            m_WaterVelPrevRT.enableRandomWrite = true;
-            m_WaterVelPrevRT.Create();
-        }
-        if (m_WaterVelRT == null) {
-            m_WaterVelRT = new RenderTexture(m_texDim[0], m_texDim[1], 0);
-            m_WaterVelRT.format = RenderTextureFormat.RGFloat;
-            m_WaterVelRT.enableRandomWrite = true;
-            m_WaterVelRT.Create();
-        }
+        m_WaterVelRT = new RenderTexture(m_texDim[0], m_texDim[1], 0);
+        m_WaterVelRT.format = RenderTextureFormat.RGFloat;
+        m_WaterVelRT.enableRandomWrite = true;
+        m_WaterVelRT.Create();
 
         //flux
-        if(m_FluxRT == null) {
-            m_FluxRT = new RenderTexture(m_texDim[0], m_texDim[1], 0);
-            m_FluxRT.format = RenderTextureFormat.ARGBFloat;
-            m_FluxRT.enableRandomWrite = true;
-            m_FluxRT.Create();
-        }
+        m_FluxRT = new RenderTexture(m_texDim[0], m_texDim[1], 0);
+        m_FluxRT.format = RenderTextureFormat.ARGBFloat;
+        m_FluxRT.enableRandomWrite = true;
+        m_FluxRT.Create();
+
+        //we did all the things!
+        m_Init = true;
     }
 
     void ReleaseData() {
-        if (m_ResultRT != null) {
-            m_ResultRT.Release();
-            m_ResultRT = null;
-        }
-        if (m_InHeightRT != null) {
+        if (m_Init) {
             m_InHeightRT.Release();
-            m_InHeightRT = null;
-        }
-
-        if (m_PrecipMaskRT != null) {
             m_PrecipMaskRT.Release();
-            m_PrecipMaskRT = null;
-        }
-
-        if (m_WaterRT != null) {
             m_WaterRT.Release();
-            m_WaterRT = null;
-        }
-
-        if (m_WaterVelRT != null) {
             m_WaterVelRT.Release();
-            m_WaterVelRT = null;
-        }
-        if (m_WaterVelPrevRT != null) {
-            m_WaterVelPrevRT.Release();
-            m_WaterVelRT = null;
-        }
-        if(m_FluxRT != null) {
             m_FluxRT.Release();
-            m_FluxRT = null;
+
+            m_Init = false;
         }
     }
 
@@ -135,38 +101,27 @@ public class Erosion : EditorWindow {
         }
     }
 
-    void SwapBuffers() {
-        // current -> previous
-        //Graphics.Blit(m_WaterRT, m_WaterPrevRT);
-        //Graphics.Blit(m_WaterVelRT, m_WaterVelPrevRT);
-    }
-
-    void DispatchComputeShader() {
+    void Simulate() {
         if (m_ComputeShader != null) {
             InitData();
             int[] numWorkGroups = { 8, 8, 1 };
-            int kidx = m_ComputeShader.FindKernel("CSMain");
+            int kidx = m_ComputeShader.FindKernel("HydraulicErosion");
 
             //set up textures for compute shader
             PrepareTextureData();
-            m_ComputeShader.SetTexture(kidx, "Result", m_ResultRT);
             m_ComputeShader.SetTexture(kidx, "InHeight", m_InHeightRT);
             m_ComputeShader.SetTexture(kidx, "PrecipMask", m_PrecipMaskRT);
-
-            //m_ComputeShader.SetTexture(kidx, "WaterPrev", m_WaterPrevRT);
+            
             m_ComputeShader.SetTexture(kidx, "Water", m_WaterRT);
-
-            m_ComputeShader.SetTexture(kidx, "WaterVelPrev", m_WaterVelPrevRT);
             m_ComputeShader.SetTexture(kidx, "WaterVel", m_WaterVelRT);
-
             m_ComputeShader.SetTexture(kidx, "Flux", m_FluxRT);
 
             m_ComputeShader.SetFloat("dt", m_DeltaTime);
             m_ComputeShader.SetFloat("precipRate", m_PrecipRate);
+            m_ComputeShader.SetFloat("flowRate", m_FlowRate);
             m_ComputeShader.SetVector("texDim", new Vector4(m_texDim[0], m_texDim[1], 0.0f, 0.0f));
 
             for (int i = 0; i < m_NumIterations; i++) {
-                //SwapBuffers();
                 m_ComputeShader.Dispatch(kidx, m_texDim[0] / numWorkGroups[0], m_texDim[0] / numWorkGroups[1], numWorkGroups[2]);
             }
 
@@ -181,27 +136,29 @@ public class Erosion : EditorWindow {
         m_PrecipitationMask = (Texture2D)EditorGUILayout.ObjectField("Precipitation Mask", m_PrecipitationMask, typeof(Texture2D));
         m_NumIterations = EditorGUILayout.IntField("# Iterations", m_NumIterations);
         m_PrecipRate = EditorGUILayout.FloatField("Precipitation Rate", m_PrecipRate);
+        m_FlowRate = EditorGUILayout.FloatField("Flow Rate", m_FlowRate);
 
         if(GUILayout.Button("Reset")) {
             ReleaseData();
             InitData();
         }
         if(GUILayout.Button("Execute")) {
-            Debug.Log("Simulating...");
-            DispatchComputeShader();
+            Simulate();
         }
 
-        int dy = 180;
+        int dy = 240;
         int x = 0;
         int y = dy;
-        //EditorGUI.DrawPreviewTexture(new Rect(x, y, m_texDim[0], m_texDim[1]), m_WaterPrevRT);
-        //y += m_texDim[1] + 2;
+
+        EditorGUI.LabelField(new Rect(x, y - 20, m_texDim[0], m_texDim[1]), "Water Level");
         EditorGUI.DrawPreviewTexture(new Rect(x, y, m_texDim[0], m_texDim[1]), m_WaterRT);
         x += m_texDim[0] + 2; y = dy;
-        EditorGUI.DrawPreviewTexture(new Rect(x, y, m_texDim[0], m_texDim[1]), m_WaterVelPrevRT);
-        y += m_texDim[0] + 2;
+        EditorGUI.LabelField(new Rect(x, y - 20, m_texDim[0], m_texDim[1]), "Water Velocity");
         EditorGUI.DrawPreviewTexture(new Rect(x, y, m_texDim[0], m_texDim[1]), m_WaterVelRT);
         x += m_texDim[0] + 2; y = dy;
+        EditorGUI.LabelField(new Rect(x, y - 20, m_texDim[0], m_texDim[1]), "Water Flux");
         EditorGUI.DrawPreviewTexture(new Rect(x, y, m_texDim[0], m_texDim[1]), m_FluxRT);
+
+        this.Repaint();
     }
 }
