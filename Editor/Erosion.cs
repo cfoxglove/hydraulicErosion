4c2ef63959b495285fd79e626fe123e4f17879e2
Chad Foxglove <chad@unity3d.com>
2018-10-19 16:24:28 -0700

    sorta kinda works

--- Editor/Erosion.cs
+++ Editor/Erosion.cs
@@ -4,20 +4,18 @@ using UnityEditor;
 public class Erosion : EditorWindow {
     private bool m_Init = false;
 
-    [SerializeField]
     private ComputeShader m_ComputeShader = null;
-    [SerializeField]
     private Terrain m_TerrainTile = null;
-    [SerializeField]
     private Texture2D m_PrecipitationMask = null;
-    [SerializeField]
-    private int m_NumIterations = 10;
-    [SerializeField]
+
+    //decent default values here
+    private int m_NumIterations = 300;
     private float m_DeltaTime = 0.01f;
-    [SerializeField]
-    private float m_PrecipRate = 0.05f;
-    [SerializeField]
-    private float m_FlowRate = -0.05f;
+    private float m_PrecipRate = 0.1f;
+    private float m_FlowRate = -0.03f;
+    private float m_SedimentCapacity = 50.0f;
+    private float m_SedimentDissolveRate = 0.01f;
+    private float m_SedimentDepositRate = 0.01f;
 
     private int[] m_texDim = { 256, 256 };
     
@@ -109,6 +107,7 @@ public class Erosion : EditorWindow {
     void Simulate() {
         if (m_ComputeShader != null) {
             InitData();
+
             int[] numWorkGroups = { 8, 8, 1 };
             int kidx = m_ComputeShader.FindKernel("HydraulicErosion");
 
@@ -121,11 +120,14 @@ public class Erosion : EditorWindow {
             m_ComputeShader.SetTexture(kidx, "WaterVel", m_WaterVelRT);
             m_ComputeShader.SetTexture(kidx, "Flux", m_FluxRT);
             m_ComputeShader.SetTexture(kidx, "Sediment", m_SedimentRT);
-            //m_ComputeShader.SetTexture(kidx, "TerrainNormals", m_TerrainNormalsRT);
 
             m_ComputeShader.SetFloat("dt", m_DeltaTime);
             m_ComputeShader.SetFloat("precipRate", m_PrecipRate);
             m_ComputeShader.SetFloat("flowRate", m_FlowRate);
+            m_ComputeShader.SetFloat("sedimentCapacity", m_SedimentCapacity);
+            m_ComputeShader.SetFloat("sedimentDissolveRate", m_SedimentDissolveRate);
+            m_ComputeShader.SetFloat("sedimentDepositRate", m_SedimentDepositRate);
+
             m_ComputeShader.SetVector("texDim", new Vector4(m_texDim[0], m_texDim[1], 0.0f, 0.0f));
 
             for (int i = 0; i < m_NumIterations; i++) {
@@ -133,7 +135,10 @@ public class Erosion : EditorWindow {
             }
 
             //Blit the output back to the terrain heightmap
-            //Graphics.Blit(m_ResultRT, m_TerrainTile.terrainData.heightmapTexture);
+            //Graphics.Blit(m_TerrainHeightRT, m_TerrainTile.terrainData.heightmapTexture);
+            Graphics.Blit(m_SedimentRT, m_TerrainTile.terrainData.heightmapTexture);
+            //m_TerrainTile.terrain.terrainData.UpdateDirtyRegion(terrainTile.clippedLocal.x, terrainTile.clippedLocal.y, terrainTile.clippedLocal.width, terrainTile.clippedLocal.height, !terrainTile.terrain.drawInstanced);
+            m_TerrainTile.terrainData.UpdateDirtyRegion(0, 0, (int)m_TerrainTile.terrainData.size.x, (int)m_TerrainTile.terrainData.size.y, true);
         }
     }
 
@@ -142,18 +147,23 @@ public class Erosion : EditorWindow {
         m_TerrainTile = (Terrain)EditorGUILayout.ObjectField("Terrain Tile", m_TerrainTile, typeof(Terrain));
         m_PrecipitationMask = (Texture2D)EditorGUILayout.ObjectField("Precipitation Mask", m_PrecipitationMask, typeof(Texture2D));
         m_NumIterations = EditorGUILayout.IntField("# Iterations", m_NumIterations);
+        m_DeltaTime = EditorGUILayout.FloatField("Simulation time step", m_DeltaTime);
         m_PrecipRate = EditorGUILayout.FloatField("Precipitation Rate", m_PrecipRate);
         m_FlowRate = EditorGUILayout.FloatField("Flow Rate", m_FlowRate);
+        m_SedimentCapacity = EditorGUILayout.FloatField("Sediment Capacity", m_SedimentCapacity);
+        m_SedimentDissolveRate = EditorGUILayout.FloatField("Sediment Dissolve Rate", m_SedimentDissolveRate);
+        m_SedimentDepositRate = EditorGUILayout.FloatField("Sediment Deposit Rate", m_SedimentDepositRate);
 
-        if(GUILayout.Button("Reset")) {
+        if (GUILayout.Button("Reset")) {
             ReleaseData();
             InitData();
         }
+
         if(GUILayout.Button("Execute")) {
             Simulate();
         }
 
-        int dy = 240;
+        int dy = 340;
         int x = 0;
         int y = dy;
 
@@ -171,7 +181,6 @@ public class Erosion : EditorWindow {
         y += m_texDim[1] + 30;
         EditorGUI.LabelField(new Rect(x, y - 20, m_texDim[0], m_texDim[1]), "Sediment");
         EditorGUI.DrawPreviewTexture(new Rect(x, y, m_texDim[0], m_texDim[1]), m_SedimentRT);
-
 
         this.Repaint();
     }
